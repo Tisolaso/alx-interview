7eced1dd4d83d21c210d37e6329aaa3c709ca808
Youssef <spanya2017@gmail.com>
2024-07-31 19:13:11 +0100

    lol

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,20 +9,29 @@ def pascal_triangle(n):
     representing the Pascal Triangle of n
     returns empty list if n <= 0
     """
-    triangle = []
+    pascal_triangle = list()
 
     if n <= 0:
-        return []
+        return pascal_triangle
 
-    for i in range(1, n + 1):
-        new = []
-        for y in range(0, i):
-            if y == 0 or y == i - 1:
-                new.append(1)
-            else:
-                old = triangle[i-2]
-                new.append(old[y - 1] + old[y])
-        
-        triangle.append(new)
-    
-    return triangle
+    # Add first 1.
+    if n > 0:
+        pascal_triangle.append([1])
+
+    # Add second line.
+    if n > 1:
+        pascal_triangle.append([1, 1])
+
+    for x in range(3, n+1):
+        pascal_triangle.append([0] * x)
+
+        # Set first and last 1
+        pascal_triangle[x-1][0] = 1
+        pascal_triangle[x-1][x-1] = 1
+
+        # Calculate middle numbers
+        for y in range(1, x-1):
+            pascal_triangle[x-1][y] = \
+                pascal_triangle[x-2][y-1] + pascal_triangle[x-2][y]
+
+    return pascal_triangle
