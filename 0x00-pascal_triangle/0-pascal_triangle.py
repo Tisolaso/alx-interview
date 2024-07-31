124d438661f95b86ce42f582d4ad38702e78392c
Youssef <spanya2017@gmail.com>
2024-07-31 18:59:18 +0100

    lol

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    triangle = []
+
+    if n <= 0:
+        return []
+
+    for i in range(1, n + 1):
+        new = []
+        for y in range(0, i):
+            if y == 0 or y == i - 1:
+                new.append(1)
+            else:
+                old = triangle[i-2]
+                new.append(old[y - 1] + old[y])
+        
+        triangle.append(new)
