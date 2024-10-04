8c19945259d6d6545a7faed3b939c3ecbb7eb6a0
Youssef <spanya2017@gmail.com>
2024-10-04 17:24:35 +0100

    lol

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,37 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
-Log parsing project
+0-stats.py
 """
+
+import re
 import sys
 
-
-def print_metrics(file_size, status_codes):
-    """
-    Print metrics using file_size, status_codes
-    """
-    print("File size: {}".format(file_size))
-    codes_sorted = sorted(status_codes.keys())
-    for code in codes_sorted:
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+size = 0
+num_lines = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+                "405": 0, "500": 0}
+pattern = (
+    r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} .*)\]'
+    r' "GET /projects/260 HTTP/1\.1"\s(200|301|[4-5]\d{2}) (\d+)'
+)
 
 
-codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
-               '403': 0, '404': 0, '405': 0, '500': 0}
-file_size_total = 0
-count = 0
-
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            try:
-                status_code = line.split()[-2]
-                if status_code in codes_count.keys():
-                    codes_count[status_code] += 1
-                # Grab file size
-                file_size = int(line.split()[-1])
-                file_size_total += file_size
-            except Exception:
-                pass
-            # print metrics if 10 lines have been read
-            count += 1
-            if count == 10:
-                print_metrics(file_size_total, codes_count)
-                count = 0
-    except KeyboardInterrupt:
-        print_metrics(file_size_total, codes_count)
-        raise
-   print_metrics(file_size_total, codes_count)
+try:
+    for input_str in sys.stdin:
+        re_match = re.match(pattern, input_str)
+        if re_match:
+            num_lines += 1
+            size += int(re_match.group(4))
+            status_codes[re_match.group(3)] += 1
+            if num_lines % 10 == 0:
+                print("File size: {}".format(size))
+                for key, value in sorted(status_codes.items()):
+                    if value > 0:
+                        print("{}: {}".format(key, value))
+except KeyboardInterrupt:
+    pass
+finally:
+    print("File size: {}".format(size))
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
